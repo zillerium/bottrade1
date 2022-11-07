7abfdbeb3dd664ea5275553995dd2d755bc606f3
Trevor <sales@merrows.co.uk>
2022-11-07 11:17:07 +0000

    rsi

--- websockets/binanceweb.js
+++ websockets/binanceweb.js
@@ -1,25 +1,61 @@
 const WebSocket = require('ws');
 const ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
-var prevSecs = 0;
-var minPrice = 0.00;
-var maxPrice = 0.00;
-var k=0;
-var prices = [];
+var prevSecs = 0; // second value for streamed txn - streamed in millisecs
+var minPrice = 0.00; // min price in a candlestick
+var maxPrice = 0.00; // max price in a candlestick
+var k=0; // number of candlesticks processed
+var numberTxns=0; // number txns in a candlestick
+var openPrice=0.00; // open price on a candlestick
+var closePrice=0.00; // close price on a candlestick
+var prevClosePrice=0.00; // prev close price on a candlestick
+var prices = []; // prices from stream
+var runCycle=10;
+var rsiPeriod=10;
+var rsiCurrent=0;
+var priceMoves=[]; // open and closes - json format
+var RSIN=5; // period for RS
+var numberPeriods=0; // number of candlesticks 
 ws.onmessage = (event) => {
-   let obj = JSON.parse(event.data);
+
+// get the streamed data	
+   let obj = JSON.parse(event.data); // data stream of prices
    console.log(JSON.stringify(obj));
-   let price = parseFloat(obj.p).toFixed(2);
+
+// get the price 
+   let price = parseFloat(obj.p).toFixed(2); // price
    console.log("price = " +price);
 
-   let orgTime = parseInt(obj.E);
+// Calc the sec value for the candlestick
+
+   let orgTime = parseInt(obj.E); // time - in millisecs
    let tradeTime = parseInt(obj.E/1000);
    console.log("trade time = " + tradeTime);
+
    let d = new Date(orgTime);
    console.log("local time in date - " + d);
    //let timeCheck = d.setUTCSeconds(orgTime);
-   let numberSecs = (d.getTime() - d.getMilliseconds())/1000;
+   let numberSecs = (d.getTime() - d.getMilliseconds())/1000; // number of secs for that candlestick
    console.log("local time in secs - " + numberSecs);
+
+   if (prevSecs == 0) {
+       // first time
+       openPrice = parseFloat(price); // first price for candlestick
+       closePrice = parseFloat(price); // init close price for candlestick
+       prevPrice = parseFloat(price); // init prev close price for candlestick
+       prevSecs = numberSecs; // sec value for candlestick
+       minPrice = parseFloat(price); // initialize to defaults
+       maxPrice = parseFloat(price);
+       numberTxns=1; // initialize to 1	   
+       k++;
+       rsiCurrentPeriod=1; // start of rsi period
+   }
+       // data streams in millisecs - only take sec blocks	   
    if (numberSecs > prevSecs) {
+	   // new candletick    
+       let priceUD = priceUpDown(closePrice, prevClosePrice); // [up, down] prices
+       if (priceMoves.length > RSIN) priceMoves.splice(0, 1); // remove first entry for that RS period
+	   priceMoves.push(priceUD);
+
        console.log("min price = " + minPrice);
        console.log("max price = " + maxPrice);
        let totPrice=parseFloat(minPrice)+parseFloat(maxPrice);
@@ -27,37 +63,135 @@ ws.onmessage = (event) => {
        let avgPrice = totPrice/2;
        let varPrice=maxPrice-avgPrice;
        console.log("avg price = " + avgPrice);
-       let pricevar = {"min":minPrice, "max":maxPrice, "avg":avgPrice, "var":varPrice};
+       let priceRatio = 0;
+       if (maxPrice > 0) { 	   
+           priceRatio = (maxPrice-avgPrice)/maxPrice;
+       
+       }
+       let rsi = 0.00;	   
+       if (priceMoves.length > RSIN) {
+           let avgP = SAMoves(priceMoves);
+	   let rs = calcRS(avgP[0]["upAvg"], calcRS[0]["downAvg"]);
+	   rsi = calcRSI(rs);
+       }
+       let pricevar = {"open":openPrice, "close": closePrice, "txns": numberTxns, "min":minPrice, "max":maxPrice, "avg":avgPrice, "var":varPrice, "ratio": priceRatio, "rsi": rsi};
        console.log("price data ===== array = " + prices);
        if (minPrice > 0) {
            prices.push(pricevar);
 
        }
        console.log("==================================== new time in secs ===================================");
-       prevSecs = numberSecs;
-       minPrice = parseFloat(price);
-       maxPrice = parseFloat(price);
+       if (priceMoves.length > RSIN) {
+           let avgP = SAMoves(priceMoves);
+       }
+       prevSecs = numberSecs; // init current sec value
+       prevClosePrice = closePrice; // save prev close price
+       minPrice = parseFloat(price); // init min price
+       minPrice = parseFloat(price); // init min price
+       maxPrice = parseFloat(price); // init max price
+       openPrice = parseFloat(price); //reset for the new candletsick
+       closePrice=parseFloat(price);  // reset for the new candlestick
+       numberTxns=1;  // initialize number of txns	   
        k++;
    } else {
+       numberTxns++;	   
+       closePrice=parseFloat(price);	   
        if (price < minPrice) minPrice =parseFloat(price);
        if (price > maxPrice) maxPrice = parseFloat(price);
 
 
    }
-   if (k>10) {
+   
+   if (k>runCycle) {
       // let pricevar = {"min":minPrice, "max":maxPrice};
      //  prices.push(pricevar);
        console.log("price data ===== array = " + prices);
+      let priceUp=0;
+      let priceDown=0;
+      let priceUpTot=0;
+      let priceDownTot=0;
+      let priceChange=0;
       for (let i=0;i < prices.length; i++) {
+	  //  {"open":20867.08,"close":20866.87,"txns":50,"min":20866.16,"max":20867.1,"avg":20866.629999999997,"var":0.47000000000116415,"ratio":0.00002252349392110855}
+          if (i>0) {
+	      priceChange = prices[i].close - prices[i-1].close;
+	      if (priceChange == 0) {
+	          priceUp=0;
+		  priceDown=0;
+	      } else {
+	          if (priceChange > 0) {
+		      priceUp=priceChange;
+		      priceDown=0;
+	          } else {
+	              priceUp=0; 
+		      priceDown=abs(priceChange);	  
+                  }
+	      }  
+	      
+	  }  
+	  if (i>rsiPeriod) {
+              
+          } 		  
+	  priceUpTot=priceUpTot+priceUp;
+	  priceDownTot=priceDownTot+priceDown;
           console.log("price data " + i + " " + JSON.stringify(prices[i]));
       }
-
+      
       process.exit();
    }
    //console.log("min price = " + minPrice);
    //console.log("max price = " + maxPrice);
 
 }
+
+
+function priceUpDown(priceCloseT, priceCloseT-1) {
+    let priceUp=0;
+    let priceDown=0;
+    priceChange = priceCloseT - pricesCloseT-1;
+    if (priceChange == 0) {
+        priceUp=0;
+        priceDown=0;
+    } else {
+        if (priceChange > 0) {
+            priceUp=priceChange;
+            priceDown=0;
+        } else {
+            priceUp=0; 
+            priceDown=abs(priceChange);	  
+        }
+    }
+    let priceUD = { "up": priceUp, "down": priceDown};
+    return priceUD;
+}
+
+function SAMoves(udMoves) {
+
+    let sumUp=0;
+    let sumDown=0;
+    for (i=0; i<udMoves.length;i++) {
+        let upP = udMoves[i]["up"];
+        let downP = udMoves[i]["down"];
+	sumUp += upP;
+	sumDown += downP;
+    }
+  //  upMoves.forEach(p => {sumUp += p;});
+  //  downMoves.forEach(p => {sumDown += p;});
+    let avgUp = sumUp/udMoves.length;
+    let avgDown = sumDown/udMoves.length;
+    let jsonAvg = {"upAvg": avgUp, "upDown": avgDown};
+    return jsonAvg; 	
+}
+
+function calcRS(avgUp, avgDown) {
+    return AvgUp/AvgDown;
+}
+
+function calcRSI(RSI) {
+    let RSI = 100 - 100/(1+RS);
+    return RSI;
+}
+
 //ws.on('message', function incoming(data) {
 //    console.log(data);
 //})
